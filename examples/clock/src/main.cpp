a22fa15b47a87387238acbc0ed0df462f0fa7127
Mateusz Adamczyk <matek_a@outlook.com>
2019-07-14 11:54:53 +0200

    Simplified example

--- examples/clock/src/main.cpp
+++ examples/clock/src/main.cpp
@@ -13,6 +13,7 @@
 #include <thread>
 #include <type_traits>
 
+
 namespace {
 class hline : public madterm::i_terminal_sequence {
 public:
@@ -103,17 +104,15 @@ int main()
     while (true) {
         auto now  = std::chrono::system_clock::now();
         auto time = std::chrono::system_clock::to_time_t(now);
-        auto tm   = *std::localtime(&time);
         std::cout << madterm::cursor::move_to(69_x, 10_y)
-                  << madterm::text::erase_characters(22)
+                  << madterm::text::erase_characters(27)
                   << madterm::cursor::move_to(69_x, 11_y)
-                  << madterm::text::erase_characters(22)
+                  << madterm::text::erase_characters(27)
                   << madterm::cursor::move_to(69_x, 12_y)
-                  << madterm::text::erase_characters(22) << madterm::text::lines
-                  << frame(68_x, 9_y, 22_x, 4_y) << madterm::text::ascii
-                  << madterm::cursor::move_to(70_x, 12_y)
-                  << std::put_time(&tm, "%Y-%m-%d %H-%M-%S")
-                  << madterm::cursor::move_to(76_x, 10_y)
+                  << madterm::text::erase_characters(27) << madterm::text::lines
+                  << frame(68_x, 9_y, 27_x, 4_y) << madterm::text::ascii
+                  << madterm::cursor::move_to(70_x, 12_y) << std::ctime(&time)
+                  << madterm::cursor::move_to(80_x, 10_y)
                   << "Time:" << std::flush;
         std::this_thread::sleep_for(std::chrono::seconds{1});
     }
